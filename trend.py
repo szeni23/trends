abd5661c7c3bccf98bb20aa4948e49849a978774
szeni23 <81260864+szeni23@users.noreply.github.com>
2024-06-22 17:30:55 +0200

    Update trend.py

--- trend.py
+++ trend.py
@@ -1,7 +1,7 @@
 import httpx
 import json
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
 
 today = datetime.today().strftime('%Y%m%d')
 
@@ -11,17 +11,16 @@ end_date_str = today
 start_date = datetime.strptime(start_date_str, '%Y%m%d')
 end_date = datetime.strptime(end_date_str, '%Y%m%d')
 
-# Load your DataFrame with the list of countries
 df_countries = pd.read_csv("countries.csv")
 
 result = []
 
-# Iterate through each country in the DataFrame
 for geo_location in df_countries['country']:
-    # Decrement the date parameter to get trends data of previous days
-    for day in range(start_date, end_date - 1, -1):
+    current_date = start_date
+    while current_date >= end_date:
+        day_str = current_date.strftime('%Y%m%d')
 
-        url = f"https://trends.google.com/trends/api/dailytrends?hl=en-{geo_location}&tz=-180&ed={day}&geo={geo_location}&hl=en-US&ns=15"
+        url = f"https://trends.google.com/trends/api/dailytrends?hl=en-{geo_location}&tz=-180&ed={day_str}&geo={geo_location}&hl=en-US&ns=15"
 
         response = httpx.get(url=url)
 
@@ -29,10 +28,10 @@ for geo_location in df_countries['country']:
             try:
                 data = json.loads(response.text.replace(")]}',", ""))
             except json.JSONDecodeError:
-                print(f"Failed to decode JSON for date: {day} in country: {geo_location}")
+                print(f"Failed to decode JSON for date: {day_str} in country: {geo_location}")
+                current_date -= timedelta(days=1)
                 continue
 
-            # Check if the list is not empty before accessing its elements
             if data["default"]["trendingSearchesDays"]:
                 date = data["default"]["trendingSearchesDays"][0]["formattedDate"]
                 for trend in data["default"]["trendingSearchesDays"][0]["trendingSearches"]:
@@ -45,16 +44,16 @@ for geo_location in df_countries['country']:
                     }
                     result.append(trend_object)
             else:
-                print(f"No data available for the date: {day} in country: {geo_location}")
+                print(f"No data available for the date: {day_str} in country: {geo_location}")
         else:
-            print(f"Failed to retrieve data for date: {day} in country: {geo_location}")
+            print(f"Failed to retrieve data for date: {day_str} in country: {geo_location}")
             print(f"Response status code: {response.status_code}")
             print(f"Response text: {response.text.strip()}")
 
-# Create a DataFrame from the result list
+        current_date -= timedelta(days=1)
+
 df_trends = pd.DataFrame(result)
 
-# Save the DataFrame to a CSV file
 df_trends.to_csv("trends.csv", index=False)
 
 print("Trends data has been saved to trends.csv")
